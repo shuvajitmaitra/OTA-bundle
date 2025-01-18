c866c1e5814d6951008fd5814713629f0880725b
shuvajitmaitra <shuvajitmaitra@gmail.com>
2025-01-18 19:32:59 +0600

    search input issue fixed

--- src/screens/Chat/NewChatScreen.js
+++ src/screens/Chat/NewChatScreen.js
@@ -1,10 +1,4 @@
-import React, {
-  useState,
-  useEffect,
-  useCallback,
-  useRef,
-  useLayoutEffect,
-} from 'react';
+import React, {useState, useEffect, useCallback, useRef} from 'react';
 import {ScrollView, StyleSheet, View, FlatList, StatusBar} from 'react-native';
 import {useDispatch, useSelector} from 'react-redux';
 import {
@@ -14,14 +8,13 @@ import {
 } from 'react-native-responsive-dimensions';
 import ChatItem from '../../components/ChatCom/ChatItem';
 import CustomFonts from '../../constants/CustomFonts';
-import {useNavigation, useRoute} from '@react-navigation/native';
+import {useNavigation} from '@react-navigation/native';
 import {useTheme} from '../../context/ThemeContext';
 import OnlineUsersItem from '../../components/ChatCom/OnlineUsersItem';
 import NoDataAvailable from '../../components/SharedComponent/NoDataAvailable';
 import {SafeAreaView} from 'react-native';
 import {loadChats} from '../../actions/chat-noti';
 import ChatHeaderFilter from '../../components/ChatCom/ChatHeaderFilter';
-import {RegularFonts} from '../../constants/Fonts';
 import Divider from '../../components/SharedComponent/Divider';
 import ChatSearchField from '../../components/ChatCom/ChatSearchField';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
@@ -29,109 +22,6 @@ import FilterOptionModal from '../../components/ChatCom/Modal/FilterOptionModal'
 import {TouchableWithoutFeedback} from 'react-native';
 import {BottomSheetModalProvider} from '@gorhom/bottom-sheet';
 import CreateCrowdModal from '../../components/ChatCom/Modal/CreateCrowdModal';
-import axiosInstance from '../../utility/axiosInstance';
-import {setChats, setGroupNameId} from '../../store/reducer/chatReducer';
-
-// const data = {
-//   user: {
-//     phone: {
-//       isVerified: true,
-//       number: '1949887896',
-//       countryCode: '880',
-//     },
-//     isEmailVerified: {
-//       status: true,
-//       token: '',
-//     },
-//     chatPermissions: {
-//       isSuspended: false,
-//       canJoinChat: true,
-//       canInitiateChat: true,
-//       canCreateChannel: true,
-//       canReadMessage: true,
-//       canSendMessage: true,
-//     },
-//     suspension: {
-//       isSuspended: false,
-//       suspendedUntil: null,
-//       reason: '',
-//     },
-//     botInfo: {
-//       branches: [],
-//       description: '',
-//       isActive: true,
-//     },
-//     personalData: {
-//       address: {
-//         country: 'Bangladesh ',
-//         city: 'Rampal',
-//         street: '318/01 Talbunia',
-//         postalCode: '9340',
-//         state: '',
-//       },
-//       socialMedia: {
-//         facebook: '',
-//         twitter: '',
-//         linkedin: '',
-//         instagram: '',
-//         github: '',
-//       },
-//       resume: '',
-//       bio: 'Hello, I am a react native developer.\n\nhttps://portal.bootcampshub.ai/dashboard',
-//     },
-//     profileStatus: {
-//       recurring: {
-//         isDailyRecurring: true,
-//         fromTime: '09:00 AM',
-//         toTime: '05:00 PM',
-//       },
-//       currentStatus: 'online',
-//     },
-//     profilePicture:
-//       'https://ts4uportal-all-files-upload.nyc3.digitaloceanspaces.com/1717578966219-Shuvajit_Maitra',
-//     lastName: 'Maitra',
-//     about:
-//       'Hello, I am a react native developer.\n\nhttps://portal.bootcampshub.ai/dashboard',
-//     type: 'marketing',
-//     password_reset_token:
-//       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvdHAiOiI1NjE3OTMiLCJlbWFpbCI6InNodXZhaml0bWFpdHJhQGdtYWlsLmNvbSIsImlhdCI6MTcxMzMzOTEyMSwiZXhwIjoxNzEzMzQyNzIxfQ.xpukx8pGk6845m1tPBYOCgyziXCjJffjHSI5oUfvFbk',
-//     pushTokens: [
-//       'ExponentPushToken[0bw6oPKQXT-JhXNPomKF9Z]',
-//       'ExponentPushToken[ErXy9rH_Icz2mtHqbkB4r3]',
-//       'ExponentPushToken[4UhWw5LjvKG55wSTP4DGHw]',
-//       'ExponentPushToken[h2BnJbEw9rJsC989zi2PNv]',
-//       'ExponentPushToken[ud12pjPG2_oDIwe6OcoMOr]',
-//       'ExponentPushToken[rMdQmdN2L9VZAqOuue6Sp0]',
-//       'ExponentPushToken[FClU_zEv39e6SJ7NN5_mwl]',
-//       'ExponentPushToken[41jk0XN6Ou4OL5ZtjzTJvn]',
-//       'ExponentPushToken[PXY3TQFBCZ40FPypGkQBoz]',
-//       'ExponentPushToken[oQ4f_RKHP8BD3WtkmV3Uty]',
-//       'ExponentPushToken[KBXAsrH6uAtJkNjiiCGIeo]',
-//       'ExponentPushToken[EfnshDAFthwLa6Md4b8T01]',
-//       'ExponentPushToken[MjUYCbCfaf29hlP98rT49X]',
-//       'ExponentPushToken[7EQ06bFVq6Ii5Dz1XTMBQ9]',
-//       'ExponentPushToken[KbZ048B7O15YA2i0iDEifF]',
-//       'ExponentPushToken[BL4gzBGWhi_gNbGZnt0ihp]',
-//     ],
-//     readingLists: [],
-//     isLockExcluded: false,
-//     _id: '658ce2bf45ea7e0019776f2f',
-//     isApproved: false,
-//     email: 'shuvajitmaitra@gmail.com',
-//     firstName: 'Shuvajit ',
-//     hash_password:
-//       '$2b$10$y9AYr3chYY4r7cmLQ9q3I.s/bmxud8Tc4HD4VCT6PM3aeYHpxXz8m',
-//     createdAt: '2023-12-28T02:51:43.264Z',
-//     updatedAt: '2024-12-12T11:46:55.866Z',
-//     id: 2210,
-//     __v: 0,
-//     lastActive: '2024-12-12T11:46:55.865Z',
-//     deviceToken:
-//       'eLgIbMzG8kjOh1Si5JlADc:APA91bEjZNagx2v0flpiCT_o6msZJR8PNq1QEoSr69V91VoVSDwf5FkNdMHgqBSPm4s6YoTpaCsaoocty7N_luCZnCrtnq2nHeOLBJi1Mppx738iJCVe3es',
-//     fullName: 'Shuvajit  Maitra',
-//   },
-//   status: 'pending',
-// };
 
 export function sortByNestedProperty(array, propertyPath, order = 'desc') {
   const getValue = (obj, path) =>
@@ -169,7 +59,6 @@ export default function NewChatScreen({navigation: {goBack}}) {
   const {user} = useSelector(state => state.auth);
   const {top} = useSafeAreaInsets();
   const navigation = useNavigation();
-  const dispatch = useDispatch();
 
   const [checked, setChecked] = useState('chats');
   const [records, setRecords] = useState([]);
@@ -181,13 +70,6 @@ export default function NewChatScreen({navigation: {goBack}}) {
     setIsCreateCrowdModalVisible(!isCreateCrowdModalVisible);
   };
   const bottomSheetRef = useRef(null);
-  // useEffect(() => {
-  //   console.log('NewChatScreen');
-  //   const filteredChats = chats?.filter(x => !x?.isArchived) || [];
-  //   setRecords(filteredChats);
-  //   setResults(filteredChats);
-  //   return () => {};
-  // }, []);
 
   useEffect(() => {
     const filteredChats = chats?.filter(x => !x?.isArchived) || [];
@@ -293,35 +175,6 @@ export default function NewChatScreen({navigation: {goBack}}) {
     bottomSheetRef.current?.present();
   }, []);
   const styles = getStyles(Colors, checked);
-
-  // const loadInitialChat = async () => {
-  //   console.log('loadChats called');
-
-  //   await axiosInstance
-  //     .get('/chat/mychats')
-  //     .then(res => {
-  //       console.log('getting all chat', JSON.stringify(res.data, null, 2));
-  //       dispatch(setChats(res.data.chats));
-  //       // // console.log('res.data.chats', JSON.stringify(res.data.chats, null, 1));
-  //       dispatch(setGroupNameId(res.data.chats));
-  //       // let totalUnread = res.data.chats?.filter(
-  //       //   chat =>
-  //       //     !chat?.myData?.isRead &&
-  //       //     chat.myData.user !== chat?.latestMessage?.sender?._id,
-  //       // )?.length;
-  //       // await Notifications.setBadgeCountAsync(totalUnread || 0);
-  //     })
-  //     .catch(err => {
-  //       // console.log(err);
-  //       console.log('err to get initial get', JSON.stringify(err, null, 2));
-  //     });
-  // };
-
-  // useEffect(() => {
-  //   loadInitialChat();
-
-  //   return () => {};
-  // }, []);
   return (
     <TouchableWithoutFeedback
       style={{zIndex: 1111111}}
@@ -336,7 +189,6 @@ export default function NewChatScreen({navigation: {goBack}}) {
               : 'light-content'
           }
         />
-        {/* <ChatTopPart handleRadioChecked={handleRadioChecked} /> */}
         <View style={styles.searchContainer}>
           <ChatSearchField
             handleFilter={handleFilter}
